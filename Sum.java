f3a864c9aebb41f839d227cfd3fc4c1d789778d9
Abhijith H <83742147+Abhijith-H13@users.noreply.github.com>
2021-11-26 16:30:30 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a,b,s;
+    System.out.println("Enter Two Numbers");
+    a = sc.nextInt();
+    b = sc.nextInt();
+    s = a + b;
+    System.out.println("Sum of "+ a+" and "+b+" is "+s);
+
+
+    
+}
+}
